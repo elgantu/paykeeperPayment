354553402416bbad55f400d7e47beb8273646654
elgantu <49722051+elgantu@users.noreply.github.com>
2023-02-16 02:29:38 +0300

    Delete controllers directory

--- controllers/BaseController.js
+++
@@ -1,6 +0,0 @@
-const SessionsModel = require('../models/SessionsModel')
-
-exports.checkSession = async (req,res) => {
-    const cheked = await SessionsModel.checkSession(req,res)
-    return cheked;
-}
